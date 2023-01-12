32865299f0aef7aa2aa8f0e9a88e9915582d2bc4
Syndicalix <sebastienspeich@ip-172-31-27-203.eu-west-3.compute.internal>
2023-01-12 09:56:42 +0000

    c'est pas d'la menthe a l'eau

---
+++ j8/assets/js/index.js
@@ -0,0 +1,16 @@
+let availableIngredients = []
+
+class Ingredient {
+    let Bacon = assets/img/bacon.png
+    let Carrots = assets/img/carrots.png
+    let Cheese = assets/img/cheese.png
+    let Egg = assets/img/egg.png
+    let Eggplant = assets/img/eggplant.png
+    let GoatCheese = assets/img/goat-cheese.png
+    let Honey = assets/img/honey.png
+    let Mushroom = assets/img/mushroom.png
+    let Olive = assets/img/olive.png
+    let Pepper = assets/img/pepper.png
+    let Potato = assets/img/potato.png
+    let Tomato = assets/img/tomato.png
+}
