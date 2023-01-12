7040b11f9252f6690cf6f6e03aa1c054cddb5142
Syndicalix <sebastienspeich@ip-172-31-27-203.eu-west-3.compute.internal>
2023-01-12 14:25:40 +0000

    les amants de saint jean

--- j8/assets/js/index.js
+++ j8/assets/js/index.js
@@ -1,18 +1,21 @@
-import { Ingredient };
+
 
 let availableIngredients = []
 
 class Ingredient {
-    set Bacon = new Ingredients("Bacon","assets/img/bacon.png");
-    set Carrots = new Ingredients("Carrots","assets/img/carrots.png");
-    set Cheese = new Ingredients("Cheese","assets/img/cheese.png");
-    set Egg = new Ingredients("Egg","assets/img/egg.png");
-    set EggPlant = new Ingredients("EggPlant","assets/img/eggplant.png");
-    set GoatCheese = new Ingredients("GoatCheese","assets/img/goat-cheese.png");
-    set Honey = new Ingredients("Honey","assets/img/honey.png");
-    set MushRoom = new Ingredients("MushRoom","assets/img/mushroom.png");
-    set Olive = new Ingredients("Olive","assets/img/olive.png");
-    set Pepper = new Ingredients("Pepper","assets/img/pepper.png");
-    set Potato = new Ingredients("Potato","assets/img/potato.png");
-    set Tomato = new Ingredients("Tomato","assets/img/tomato.png");
+    let Bacon = new Ingredients("Bacon","assets/img/bacon.png");
+    let Carrots = new Ingredients("Carrots","assets/img/carrots.png");
+    let Cheese = new Ingredients("Cheese","assets/img/cheese.png");
+    let Egg = new Ingredients("Egg","assets/img/egg.png");
+    let EggPlant = new Ingredients("EggPlant","assets/img/eggplant.png");
+    let GoatCheese = new Ingredients("GoatCheese","assets/img/goat-cheese.png");
+    let Honey = new Ingredients("Honey","assets/img/honey.png");
+    let MushRoom = new Ingredients("MushRoom","assets/img/mushroom.png");
+    let Olive = new Ingredients("Olive","assets/img/olive.png");
+    let Pepper = new Ingredients("Pepper","assets/img/pepper.png");
+    let Potato = new Ingredients("Potato","assets/img/potato.png");
+    let Tomato = new Ingredients("Tomato","assets/img/tomato.png");
 };
+
+console.log(Bacon);
+console.log(Carrots);
